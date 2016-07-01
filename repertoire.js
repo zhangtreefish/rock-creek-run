24b6b52de0deaf2c06a920ba44e6acd64901307a
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-07-01 18:20:20 -0500

    test

--- repertoire.js
+++ repertoire.js
@@ -15,7 +15,10 @@ MongoClient.connect("mongodb://lz:S4M3K4@ds035348.mlab.com:35348/projects", func
 	}
 	var piece1 = {
 		title: 'The Gondola',
-		composer: 'Faber',
+		composer: {
+			name: 'Faber',
+			years: ''
+		},
 		genre: 'Contemporary',
 		arranger: '',
 		book: 'Lesson Book Level 4, Fabers',
@@ -23,7 +26,10 @@ MongoClient.connect("mongodb://lz:S4M3K4@ds035348.mlab.com:35348/projects", func
 		};
 	var piece2 = {
 		title: 'Menuet',
-		composer: 'J.S. Bach(1685-1750)',
+		composer:  {
+			name: 'J.S. Bach',
+			years: '1685-1750'
+		},
 		genre: 'Baroque',
 		arranger: '',
 		book: "J.S. Bach, Selections from Anna Magalena's Notebook, Edited by Willard A. Palmer",
@@ -31,7 +37,10 @@ MongoClient.connect("mongodb://lz:S4M3K4@ds035348.mlab.com:35348/projects", func
 		};
 	var piece3 = {
 		title: 'Sonatina, Op. 36, No. 1',
-		composer: 'Muzio Clementi(1752-1832)',
+		composer: {
+			name: 'Muzio Clementi',
+			years: '1752-1832'
+		},
 		genre: 'Classical',
 		arranger: '',
 		book: 'Artist Piano Sonatinas, Book Two Intermediate, Compiled and edited by Nancy and Randall Faber',
@@ -39,7 +48,10 @@ MongoClient.connect("mongodb://lz:S4M3K4@ds035348.mlab.com:35348/projects", func
 		};
 	var piece4 = {
 		title: 'Ballad',
-		composer: 'Johann Burgmuller(1806-1874)',
+		composer: {
+			name: 'Johann Burgmuller',
+			years: '1806-1874'
+		},
 		genre: 'Classical',
 		arranger: 'None',
 		book: 'Lesson Book Level 5, Fabers',
@@ -47,20 +59,23 @@ MongoClient.connect("mongodb://lz:S4M3K4@ds035348.mlab.com:35348/projects", func
 		};
 	var piece5 = {
 		title: 'Duel of the Fates',
-		composer: 'John Williams',
+		composer: {
+			name: 'John Williams',
+			years: ''
+		},
 		genre: 'Contemporary',
 		arranger: '',
 		book: 'Star Wars, Music by John Williams',
 		pages: [12, 15],
 		};
-	db.collection('repertoire').remove();
+	//db.collection('repertoire').remove();
 	//db.collection('repertoire').createIndex({"title": 1}, { "unique": true });
     db.collection('repertoire').insertMany([piece1, piece2, piece3, piece4, piece5], function(err, result) {
     	if(err){
     		console.log('err at insert', err);
     		process.exit(1);
     	}
-    	var query = { genre: 'Classical'};
+    	var query = { 'composer.name': 'John Williams'};
     	db.collection('repertoire').find(query).toArray(function(err, docs) {
     		if(err){
 	    		console.log('err at find', err);
